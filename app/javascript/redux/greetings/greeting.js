1e6a2f10fe97a63dbc0f1226884953e6ddc494c3
Menekam Rudy <menekamrudy@gmail.com>
2023-06-28 23:03:17 +0100

    Fix error module not found for @hotwired stimulus

--- app/javascript/redux/greetings/greeting.js
+++ app/javascript/redux/greetings/greeting.js
@@ -4,8 +4,9 @@ const GET_GREETING = 'hello-rails-react/greeting/GET_GREETING';
 const initialState = '';
 
 export const fetchGreeting = createAsyncThunk(GET_GREETING, async () => {
-  const data = await fetch('api/greeting');
+  const data = await fetch('api/message');
   const response = await data.json();
+  console.log(response);
   return response;
 });
 
